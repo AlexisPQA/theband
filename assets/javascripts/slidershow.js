2c04d335e830104e755e164a0955dab78c46c82f
AlexisPQA <ppaax1@gmail.com>
2021-10-22 22:48:18 +0700

    init & completed header ( navbar )

---
+++ assets/javascripts/slidershow.js
@@ -0,0 +1,16 @@
+let head = document.getElementsByClassName("header")[0];
+let content = document.getElementsByClassName("content")[0];
+console.log(head)
+content.style.top = head.offsetHeight + "px";
+let index = 1;
+function autoSlideShow() {
+    const slide = document.getElementsByClassName("slide")
+    slide[0].style.display = "block"
+    setInterval(() =>{
+        slide[ (index-1 ) < 0 ? slide.length-1 : index-1].style.display = "none";
+        slide[index].style.display = "block";
+        index = (index+1) > 2 ? 0 : index + 1
+    },2000)
+}
+
+autoSlideShow()
