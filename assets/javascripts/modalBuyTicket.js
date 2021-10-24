b244c0e7ab797493c62ab9a836f4906ead404aac
AlexisPQA <ppaax1@gmail.com>
2021-10-24 13:32:05 +0700

    contact section: done

--- assets/javascripts/modalBuyTicket.js
+++ assets/javascripts/modalBuyTicket.js
@@ -1,11 +1,15 @@
 const modal = document.getElementsByClassName("modal-buy-ticket")[0];
-
+const modalContainer = document.getElementsByClassName("modal-container")[0]
 function onClickBuyTicket (){
-    console.log("show")
     modal.style.display ="block"
 }
 
 function onClickCloseModal(){
-    console.log("closed")
     modal.style.display = "none"
 }
+
+modal.addEventListener('click', onClickCloseModal)
+
+modalContainer.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
