30939f621fe89445c9d377367d0ae06563d85f23
AlexisPQA <ppaax1@gmail.com>
2021-10-26 00:42:42 +0700

    fix submenu of more-menu

--- assets/javascripts/MenuMobile.js
+++ assets/javascripts/MenuMobile.js
@@ -1,5 +1,6 @@
 const menuMobile = document.getElementsByClassName("menu-mobile-btn")[0];
 const navigation = document.getElementsByClassName("navigation")[0];
+const subMenu = document.getElementsByClassName("sub-menu")[0];
 let menus = document.getElementsByClassName("menu");
 menuMobile.addEventListener('click',onClickMenuMobile);
 let isShow = false;
@@ -12,6 +13,9 @@ for(let i = 0; i < menus.length; i++) {
     if(menus[i].localName != "div"){
         menus[i].onclick = onClickMenu;
     }
+    else{
+        menus[i].onclick = onClickMore;
+    }
 }
 
 //hide menu when click on menu item
@@ -19,4 +23,14 @@ function onClickMenu() {
     if(isShow){
         navigation.style.display = "none";  
     }
+}
+
+let isShowMore = false;
+function onClickMore() {
+    if(isShowMore){
+        subMenu.style.display = "none"
+    }else{
+        subMenu.style.display = "flex"
+    }
+    isShowMore = !isShowMore;
 }
