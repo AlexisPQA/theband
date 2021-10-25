a7358572e8f5479be2783dcf60f1675ff5b233d9
AlexisPQA <ppaax1@gmail.com>
2021-10-25 23:55:05 +0700

    responsive mobile

---
+++ assets/javascripts/MenuMobile.js
@@ -0,0 +1,21 @@
+const menuMobile = document.getElementsByClassName("menu-mobile-btn")[0];
+const navigation = document.getElementsByClassName("navigation")[0];
+let menus = document.getElementsByClassName("menu");
+menuMobile.addEventListener('click',onClickMenuMobile);
+console.log(menus)
+let isShow = false;
+function onClickMenuMobile() {
+    navigation.style.display = isShow===false ? 'flex' : 'none';
+    isShow = !isShow;   
+}
+
+for(let i = 0; i < menus.length; i++) {
+    if(menus[i].localName != "div"){
+        menus[i].onclick = onClickMenu;
+    }
+}
+
+//hide menu when click on menu item
+function onClickMenu() {
+    navigation.style.display = "none";
+}
